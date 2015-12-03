0b7ea5929fe3e58cc013e429451acb5324cd1cdf
neutralevil <liubin.net@gmail.com>
2015-12-03 21:28:30 +0800

    Progress to chapter 2 step 13

--- cpp-tdd/blocks/neutralevil/c2/Soundex.h
+++ cpp-tdd/blocks/neutralevil/c2/Soundex.h
@@ -5,7 +5,10 @@
 class Soundex {
 public:
     std::string encode(const std::string& word) const {
-        return zeroPad(word);
+        auto encoded = word.substr(0, 1);
+        if (word.length() > 1)
+            encoded += "1";
+        return zeroPad(encoded);
     }
 
 private:
