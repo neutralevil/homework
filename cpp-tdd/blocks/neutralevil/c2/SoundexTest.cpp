c245bf0e0ec03e2dee5f9dd1800f834658d8ab14
neutralevil <liubin.net@gmail.com>
2015-12-03 20:34:09 +0800

    Progress to chapter 2 step 10

--- cpp-tdd/blocks/neutralevil/c2/SoundexTest.cpp
+++ cpp-tdd/blocks/neutralevil/c2/SoundexTest.cpp
@@ -2,21 +2,29 @@
 
 class Soundex {
 public:
-    std::string encode(const std::string& word) {
+    std::string encode(const std::string& word) const {
+        return zeroPad(word);
+    }
+
+private:
+    std::string zeroPad(const std::string& word) const {
         return word + "000";
     }
 };
 
 using namespace testing;
 
-TEST(SoundexEncoding, RetainsSoleLetterOfOneLetterWord) {
+class SoundexEncoding : public Test {
+public:
     Soundex soundex;
+};
+
+TEST_F(SoundexEncoding, RetainsSoleLetterOfOneLetterWord) {
     auto encoded = soundex.encode("A");
     ASSERT_THAT(encoded, Eq("A000"));
 }
 
-TEST(SoundexEncoding, PadsWithZerosToEnsureThreeDigits) {
-    Soundex soundex;
+TEST_F(SoundexEncoding, PadsWithZerosToEnsureThreeDigits) {
     auto encoded = soundex.encode("I");
     ASSERT_THAT(encoded, Eq("I000"));
 }
