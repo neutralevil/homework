a529699d5ef13600bfbb0b82f0114406c963a826
neutralevil <liubin.net@gmail.com>
2015-12-04 20:21:40 +0800

    Progress to chapter 2 step 20 (skipped 19)

--- cpp-tdd/blocks/neutralevil/c2/Soundex.h
+++ cpp-tdd/blocks/neutralevil/c2/Soundex.h
@@ -1,6 +1,7 @@
 #ifndef CPP_TDD_SOUNDEX_H
 #define CPP_TDD_SOUNDEX_H
 #include <string>
+#include <unordered_map>
 
 class Soundex {
 public:
@@ -22,9 +23,16 @@ private:
     }
 
     std::string encodeDigit(char letter) const {
-        if (letter == 'c')
-            return "2";
-        return "1";
+        std::unordered_map<char, std::string> encodings = {
+                {'b', "1"}, {'f', "1"}, {'p', "1"}, {'v', "1"},
+                {'c', "2"}, {'g', "2"}, {'j', "2"}, {'k', "2"}, {'q', "2"},
+                            {'s', "2"}, {'x', "2"}, {'z', "2"},
+                {'d', "3"}, {'t', "3"},
+                {'l', "4"},
+                {'m', "5"}, {'n', "5"},
+                {'r', "6"},
+        };
+        return encodings.find(letter)->second;
     }
 
     std::string zeroPad(const std::string& word) const {
