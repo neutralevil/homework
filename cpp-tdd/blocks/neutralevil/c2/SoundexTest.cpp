966abf163e632f34faa62a0437c964c9fbc04eec
neutralevil <liubin.net@gmail.com>
2015-12-04 20:39:29 +0800

    Progress to chapter 2 step 21

--- cpp-tdd/blocks/neutralevil/c2/SoundexTest.cpp
+++ cpp-tdd/blocks/neutralevil/c2/SoundexTest.cpp
@@ -19,3 +19,7 @@ TEST_F(SoundexEncoding, PadsWithZerosToEnsureThreeDigits) {
 TEST_F(SoundexEncoding, ReplacesConsonantsWithAppropriateDigits) {
     ASSERT_THAT(soundex.encode("Ax"), Eq("A200"));
 }
+
+TEST_F(SoundexEncoding, IgnoresNonAlphabetics) {
+    ASSERT_THAT(soundex.encode("A#"), Eq("A000"));
+}
