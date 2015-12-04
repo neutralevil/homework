71057226629f56311e82f7ac8eaf265a2c338fb2
neutralevil <liubin.net@gmail.com>
2015-12-04 21:20:57 +0800

    Progress to chapter 2 step 25

--- cpp-tdd/blocks/neutralevil/c2/Soundex.h
+++ cpp-tdd/blocks/neutralevil/c2/Soundex.h
@@ -21,8 +21,6 @@ private:
     }
 
     std::string encodedDigits(const std::string& word) const {
-        if (word.empty())
-            return "";
         std::string encoding;
         for (auto letter: word)
             encoding += encodeDigit(letter);
